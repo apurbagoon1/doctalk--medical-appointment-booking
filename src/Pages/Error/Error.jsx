e18a0c346ee4d29d03ceb9cc43bd242881be3ea5
apurbagoon1 <apurbagoon1@gmail.com>
2025-04-23 22:39:41 +0600

    edited a portion of error file

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -8,22 +8,22 @@ const Error = () => {
     return (
         <div>
             <Navbar />
-            <div className="flex flex-col items-center justify-center p-4 text-center">
+            <div className="flex flex-col items-center justify-center p-5 text-center">
                 <img
                     src={errorImage}
                     alt="Error"
-                    className="max-w-xl rounded-xl mb-4"
+                    className="max-w-xl rounded-xl mb-6"
                 />
-                <h2 className="text-4xl font-bold text-red-400 mb-3">Oops! Page Not Found</h2>
+                <h2 className="text-4xl font-bold text-red-400 mb-4">Oops! Page Not Found</h2>
                 <p className="text-lg opacity-60 max-w-2xl mb-2">
                     The page you’re trying to access doesn’t exist or may have been moved.
                 </p>
-                <p className="text-sm opacity-50 mb-4">
+                <p className="text-sm opacity-50 mb-5">
                     You can try reloading the page — sometimes that works. Or, click the button below to return to the homepage.
                 </p>
                 <Link
                     to="/"
-                    className="inline-flex items-center gap-2 bg-[#176AE5] text-white font-semibold px-6 py-3 rounded-3xl hover:bg-[#145dc4] transition-all"
+                    className="inline-flex items-center gap-2 bg-[#176AE5] text-white font-semibold px-6 py-3 rounded-3xl"
                 >
                     <FaArrowLeft /> Go Home
                 </Link>
